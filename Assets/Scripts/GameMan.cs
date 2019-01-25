0adc9f335e19fb33dea666f87b923add10455b9e
Zac <chikanz@outlook.com>
2019-01-26 10:41:33 +1100

    Basic movement, fog, poly

---
+++ Assets/Scripts/GameMan.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameMan : MonoBehaviour
+{
+    public GameObject[] Lanes;
+    private player[] players;
+    private int[] playerLanes;
+
+	// Use this for initialization
+	void Start ()
+    {
+        players = GetComponentsInChildren<player>();
+        playerLanes = new int[players.Length];
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public Vector3 GetLanePosition(int lane, int player)
+    {
+        playerLanes[player] = lane; //Update lane in manager
+
+        if (player == 0)
+        {           
+            return Lanes[lane].transform.position - (Vector3.left * players[player == 0 ? 1 : 0].GetComponentInChildren<BoxCollider>().bounds.extents.magnitude);
+        }
+        else
+        {            
+            return Lanes[lane].transform.position; //normal pos
+        }
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public int playerIndex = 0;
+    private GameMan manager;
+    private int laneIndex = 0; //current lane
+    public float lerpSpeed;    
+
+    private KeyCode GetLeft => playerIndex == 0 ? KeyCode.A : KeyCode.LeftArrow;
+    private KeyCode GetRight => playerIndex == 0 ? KeyCode.D : KeyCode.RightArrow;
+
+    // Use this for initialization
+    void Start ()
+    {
+        manager = GetComponentInParent<GameMan>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(GetLeft))
+        {
+            laneIndex -= 1;
+        }
+
+        if (Input.GetKeyDown(GetRight))
+        {
+            laneIndex += 1;            
+        }
+
+        laneIndex = Mathf.Clamp(laneIndex, 0, manager.Lanes.Length - 1);
+
+        transform.position = Vector3.Lerp(transform.position, manager.GetLanePosition(laneIndex, playerIndex), lerpSpeed);
+
+    }
+}
