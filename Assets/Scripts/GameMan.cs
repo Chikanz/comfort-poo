4c63c5116406e745c1bf100303c36ed3a4e552d8
Zac <chikanz@outlook.com>
2019-01-26 18:57:02 +1100

    POst process evertything grey now

--- Assets/Scripts/GameMan.cs
+++ Assets/Scripts/GameMan.cs
@@ -9,6 +9,18 @@ public class GameMan : MonoBehaviour
     private player[] players;
     private int[] playerLanes;
 
+    public float LampTime;
+
+    private int[,] placementMask =
+    {
+        {1,0,0,0,0}, //Lamppost (not used)
+        {0,1,0,0,0}, //Chair
+        {0,1,0,0,0}, //Mailbox
+        {1,1,1,1,1}, //test
+        {0,1,0,0,0}, //Trash can
+        {0,0,1,1,1}, //Doggie
+    };
+
     public float spawnZ = 34;
 
     public Transform mover;
@@ -24,16 +36,37 @@ public class GameMan : MonoBehaviour
         playerLanes = new int[players.Length];
 
         Invoke(nameof(SpawnObsticle), Random.Range(ObsticleTimer.x, ObsticleTimer.y));
+        InvokeRepeating(nameof(SpawnLampPost), 0, LampTime);
+    }
+
+    void SpawnLampPost()
+    {
+        SpawnObsticleInLane(Obsticles[0], 0);
     }
 
     void SpawnObsticle()
     {
-        var g = Obsticles[Random.Range(0, Obsticles.Length)];
+        bool b = false;
+        int laneIndex = 0;
+        int obsIndex = Random.Range(1, Obsticles.Length);
+        while (!b)
+        {
+            laneIndex = Random.Range(0, Lanes.Length);
+            b = placementMask[obsIndex, laneIndex] == 1;
+        }
+        
+        GameObject g = Obsticles[obsIndex];
+        SpawnObsticleInLane(g, laneIndex);
+        Invoke(nameof(SpawnObsticle), Random.Range(ObsticleTimer.x, ObsticleTimer.y));
+    }
+
+    void SpawnObsticleInLane(GameObject g,int laneIndex)
+    {
         //ohhhhhh yeah
-        var instance = Instantiate(g, Lanes[Random.Range(0, Lanes.Length)].transform.position + Vector3.left * players[0].GetComponentInChildren<BoxCollider>().bounds.extents.magnitude/2, g.transform.rotation); 
+        var instance = Instantiate(g, Lanes[laneIndex].transform.position +
+            Vector3.left * players[0].GetComponentInChildren<BoxCollider>().bounds.extents.magnitude / 2, g.transform.rotation);
         var p = instance.transform.position;
         instance.transform.position = new Vector3(p.x, p.y, spawnZ);
-        Invoke(nameof(SpawnObsticle), Random.Range(ObsticleTimer.x, ObsticleTimer.y));
         instance.transform.SetParent(mover);
     }
 	
